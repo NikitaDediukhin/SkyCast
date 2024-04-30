5df293f8ca601209ee58f294016b523a26cfa07a
Nikita <dedukhin.nickita@ya.ru>
2024-04-30 21:20:13 +0300

    Updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,19 +40,28 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
-    implementation("com.google.android.gms:play-services-location:19.0.1")
-    implementation(project(":data"))
-    implementation(project(":domain"))
-    implementation("com.squareup.retrofit2:adapter-java8:2.9.0")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.github.bumptech.glide:glide:4.16.0")
-    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-    implementation("com.facebook.shimmer:shimmer:0.5.0")
     implementation("androidx.core:core:1.12.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    //Fragments
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    //Location
+    implementation("com.google.android.gms:play-services-location:19.0.1")
+    // modules
+    implementation(project(":data"))
+    implementation(project(":domain"))
+    //Retrofit2
+    implementation("com.squareup.retrofit2:adapter-java8:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    //Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    //swiperefreshlayout
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    //shimmer
+    implementation("com.facebook.shimmer:shimmer:0.5.0")
 }
